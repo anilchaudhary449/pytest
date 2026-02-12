8ca2d0ebc5e4915e70ce362b804c0bf7e426d90c
ANILCHAUDHARY <anil.bhumal@gmail.com>
2026-02-12 13:32:56 +0545

    feat: Add parameterized addition tests using pytest.

---
+++ parametrize/parametrized_test_cases.py
@@ -0,0 +1,14 @@
+import pytest
+
+Additions = [
+    (2,4,6),
+    (1,2,3),
+    (3,4,7),
+    (4,5,9),
+    (5.0,6.0,11.0),
+    (6.0,7.0,13.0)
+]
+
+@pytest.mark.parametrize("a,b,expected", Additions)
+def test_add(a,b,expected):
+    assert a + b == expected
